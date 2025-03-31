0ad91a9bafaf5f9d22f5715771351944ee7174b6
ryanbromati <ryan.bromati@hotmail.com>
2025-03-31 00:30:06 -0300

    feat: add suporte a PostgreSQL e Redis na configuração do aplicativo

--- src/MapShare.AppHost/Program.cs
+++ src/MapShare.AppHost/Program.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var username = builder.AddParameter("username", secret: true);
+var password = builder.AddParameter("password", secret: true);
+
+var postgresql = builder.AddPostgres("postgresql", username, password).WithPgAdmin(pgadmin => pgadmin.WithHostPort(5432));
+postgresql.AddDatabase("postgresdb");
+
+var redis = builder.AddRedis("redis").WithRedisInsight(redisInsight => redisInsight.WithHostPort(5540));
+
 builder.Build().Run();
