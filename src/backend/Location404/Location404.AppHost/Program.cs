c2f22c6e52199d7fb552e49116f176e7e416c309
ryanbromati <ryan.bromati@hotmail.com>
2025-04-12 19:40:38 -0300

    reactor: rename identity service

--- src/backend/Location404/Location404.AppHost/Program.cs
+++ src/backend/Location404/Location404.AppHost/Program.cs
@@ -2,6 +2,7 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Location404_IdentityService>("identity-service");
+var identityService = builder.AddProject<Location404_IdentityService>("identity-service");
+// identityService.WithUrl("https://identity-service.localhost:5001/");
 
 builder.Build().Run();
