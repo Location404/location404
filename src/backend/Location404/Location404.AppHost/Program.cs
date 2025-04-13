2363af2cc403dd1a3a8e317983e6030c581e580d
ryanbromati <ryan.bromati@hotmail.com>
2025-04-13 00:43:56 -0300

    començando do zero de novo, ideias surgindo...

--- src/backend/Location404/Location404.AppHost/Program.cs
+++
@@ -1,7 +0,0 @@
-using Projects;
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-var identityService = builder.AddProject<Location404_IdentityService_API>("identity-service");
-
-builder.Build().Run();
