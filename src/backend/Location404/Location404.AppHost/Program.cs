be2a6daf307dbb0341886b4a1d7070a3aba2f893
ryanbromati <ryan.bromati@hotmail.com>
2025-04-12 21:36:26 -0300

    feat: adicionar serviço de identity

--- src/backend/Location404/Location404.AppHost/Program.cs
+++ src/backend/Location404/Location404.AppHost/Program.cs
@@ -2,7 +2,6 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var identityService = builder.AddProject<Location404_IdentityService>("identity-service");
-// identityService.WithUrl("https://identity-service.localhost:5001/");
+var identityService = builder.AddProject<Location404_IdentityService_API>("identity-service");
 
 builder.Build().Run();
